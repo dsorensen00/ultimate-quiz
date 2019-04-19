ae50e45277beb87e2df53d42610d64aeedf76ff7
Damon Sorensen <damonsorensen@gmail.com>
2019-04-19 12:43:43 -0600

    Fixed Grading

--- grading.js
+++ grading.js
@@ -1,30 +1,46 @@
 
-export default ()=>{
+grader=()=>{
 
 
-    const question1 = document.quiz.question1.value;
-	const question2 = document.quiz.question2.value;
-	const question3 = document.quiz.question3.value;
-    const correct = 0;
+    let question1 = document.quiz.question1.value;
+	let question2 = document.quiz.question2.value;
+	let question3 = document.quiz.question3.value;
+    let correct = 0;
 
 
-    if(question1 === 'Darth Vader'){
-        correct++
+    if(question1 == 'Darth Vader'){
+        correct++;
     }
-    if(question2 === 'Apple'){
-        correct++
+    if(question2 == 'Apple'){
+        correct++;
     }
-    if(question3 === 'Subaru'){
-        correct++
+    if(question3 == 'Subaru'){
+        correct++;
     }
-    if(correct === 3){
-        return(
-            <>
-                
+    
+    let pictures = ['img/win.gif', 'img/meh.jpeg', 'img/lose.gif'];
+	let messages = ['Great job!', "That's just okay", 'You really need to do better'];
+	let score= null;
+    
+    
+    if (correct == 0) {
+		score = 2;
+	}
 
-            </>
-        )
-    }
+	if (correct > 0 && correct < 3) {
+		score = 1;
+	}
+
+	if (correct == 3) {
+		score = 0;
+	}
+
+	document.getElementById("after_submit").style.visibility = "visible";
+
+	document.getElementById("message").innerHTML = messages[score];
+	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+	document.getElementById("picture").src = pictures[score];
+	
     
     
 
